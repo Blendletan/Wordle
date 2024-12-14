b2fcdb570451c8d44e967baf2e987aa1477ed33e
Blendletan <149725846+Blendletan@users.noreply.github.com>
2024-12-14 15:51:26 -0700

    Add project files.

---
+++ Wordle/Program.cs
@@ -0,0 +1,107 @@
+﻿namespace Wordle
+{
+    using WordleEngine;
+    internal class Program
+    {
+        //place a dictionary file named words.txt in the same folder as the executable
+        const string filePath = "words.txt";
+        static void Main(string[] args)
+        {
+            WordleEngine engine;
+            using (var reader = new StreamReader(filePath))
+            {
+                engine = new WordleEngine(reader);
+            }
+            while (true)
+            {
+                PlayWordle(engine);
+                engine.Reset();
+            }
+        }
+        static void PlayWordle(WordleEngine engine)
+        {
+            Console.WriteLine($"First guess should be {engine.FirstGuess()}");
+            for(int i = 0; i < 5; i++)
+            {
+                var nextResult = GetNextResult(engine);
+                engine.UpdateInfo(nextResult.Item1, nextResult.Item2);
+                var guessInfo = engine.NextGuess();
+                WriteGuess(guessInfo);
+                if (guessInfo.NumberOfPossibilites == 1)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("Wordle finished, press ENTER to play again");
+            Console.ReadLine();
+        }
+        static void WriteGuess(WordleGuessInformation information)
+        {
+            Console.WriteLine($"Time to make guess was {information.ElapsedMilliseconds}");
+            Console.WriteLine($"You should guess {information.BestGuess}");
+            Console.WriteLine($"Second best guess should be {information.SecondBestGuess}");
+            Console.WriteLine($"Number of possibilities is {information.NumberOfPossibilites}");
+        }
+        static bool VerifyWord(string input, WordleEngine engine)
+        {
+            return engine.WordAdmissible(input);
+        }
+        static LetterMatchOutcome? VerifyOutcome(string? input)
+        {
+            if (input == null)
+            {
+                return null;
+            }
+            if (input.ToLower() == "yellow")
+            {
+                return LetterMatchOutcome.Yellow;
+            }
+            if (input.ToLower() == "green")
+            {
+                return LetterMatchOutcome.Green;
+            }
+            if (input.ToLower() == "grey")
+            {
+                return LetterMatchOutcome.Grey;
+            }
+            return null;
+        }
+        static LetterMatchOutcome GetNextLetterOutcome(int letterNumber)
+        {
+            LetterMatchOutcome? attemptedOutcome = null;
+            while (true)
+            {
+                Console.WriteLine($"Did letter number {letterNumber + 1} match?  Respond with yellow, green, or grey");
+                string? input = Console.ReadLine()?.ToLower();
+                attemptedOutcome = VerifyOutcome(input);
+                if (attemptedOutcome != null)
+                {
+                    break;
+                }
+            }
+            return attemptedOutcome.Value;
+        }
+        static (string, WordleMask) GetNextResult(WordleEngine engine)
+        {
+            string? attemptedInput = null;
+            while (true)
+            {
+                Console.WriteLine("Please type in the word you guessed and press enter");
+                attemptedInput = Console.ReadLine()?.ToLower();
+                if (attemptedInput != null)
+                {
+                    if (VerifyWord(attemptedInput, engine))
+                    {
+                        break;
+                    }
+                }
+            }
+            var outcome = new List<LetterMatchOutcome>();
+            for (int i = 0; i < WordleEngine.wordSize; i++)
+            {
+                outcome.Add(GetNextLetterOutcome(i));
+            }
+            return (attemptedInput, new WordleMask(outcome));
+        }
+    }
+}
---
+++ WordleEngine/WordleEngine.cs
@@ -0,0 +1,238 @@
+﻿using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography;
+namespace WordleEngine
+{
+    public class WordleEngine
+    {
+        Stopwatch sw;
+        public const int wordSize = 5;
+        readonly List<string> permissibleWords;
+        List<string> possibleWords;
+        const string firstGuess = "tares";
+        public WordleEngine(TextReader reader)
+        {
+            sw = new Stopwatch();
+            possibleWords = new List<string>();
+            permissibleWords = new List<string>();
+            string? nextLine = reader.ReadLine();
+            if (nextLine == null)
+            {
+                throw new Exception("Invalid Dictionary file");
+            }
+            while (nextLine != null)
+            {
+                if (nextLine.Count() == wordSize)
+                {
+                    permissibleWords.Add(nextLine.ToLower());
+                    possibleWords.Add(nextLine.ToLower());
+                }
+                nextLine = reader.ReadLine();
+            }
+        }
+        public void Reset()
+        {
+            possibleWords = new List<string>(permissibleWords);
+        }
+        public bool WordAdmissible(string input)
+        {
+            if (permissibleWords.Contains(input))
+            {
+                return true;
+            }
+            return false;
+        }
+        public string FirstGuess()
+        {
+            return firstGuess;
+        }
+        public WordleGuessInformation NextGuess()
+        {
+            WordleGuessInformation output;
+            sw.Restart();
+            string? bestGuess = null;
+            double? bestGuessValue = null;
+            string? secondBestGuess = null;
+            if (possibleWords.Count == 1)
+            {
+                sw.Stop();
+                output = new WordleGuessInformation(possibleWords[0], null, sw.ElapsedMilliseconds, 1);
+                return output;
+            }
+            foreach (var word in permissibleWords)
+            {
+                double expectedWordValue = 0;
+                var maskProbabilities = GetProbabilites(word);
+                foreach (var maskProbability in maskProbabilities)
+                {
+                    double probabilityOfGuess = maskProbability.Value;
+                    double numberOfWordsRuledOut = (1 - probabilityOfGuess) * possibleWords.Count;
+                    expectedWordValue += probabilityOfGuess * numberOfWordsRuledOut;
+                }
+                if (bestGuess == null)
+                {
+                    bestGuess = word;
+                    bestGuessValue = expectedWordValue;
+                }
+                else if (expectedWordValue > bestGuessValue)
+                {
+                    secondBestGuess = bestGuess;
+                    bestGuess = word;
+                    bestGuessValue = expectedWordValue;
+                }
+            }
+            if (bestGuess == null)
+            {
+                throw new Exception("Couldn't make a guess");
+            }
+            sw.Stop();
+            output = new WordleGuessInformation(bestGuess, secondBestGuess, sw.ElapsedMilliseconds, possibleWords.Count);
+            return output;
+        }
+        public void UpdateInfo(string guessedWord, WordleMask guessOutcome)
+        {
+            var toRemove = new List<string>();
+            foreach (var word in possibleWords)
+            {
+                WordleMask testOutcome = Compare(guessedWord, word);
+                if (guessOutcome.GetHashCode() != testOutcome.GetHashCode())
+                {
+                    toRemove.Add(word);
+                }
+            }
+            foreach (var word in toRemove)
+            {
+                possibleWords.Remove(word);
+            }
+        }
+        Dictionary<int,double> GetProbabilites(string guess)
+        {
+            Dictionary<int, double> outputs = new Dictionary<int, double>();
+            foreach(var answer in possibleWords)
+            {
+                WordleMask mask = Compare(guess, answer);
+
+                if (outputs.ContainsKey(mask.GetHashCode()) == false)
+                {
+                    outputs.Add(mask.GetHashCode(), 0);
+                }
+                outputs[mask.GetHashCode()]++;
+                outputs[mask.GetHashCode()] /= possibleWords.Count;
+            }
+            return outputs;
+        }
+
+        static WordleMask Compare(string guess, string trueAnswer)
+        {
+            var outcomes = new List<LetterMatchOutcome>();
+            for (int i = 0; i < wordSize; i++)
+            {
+                char nextGuessChar = guess[i];
+                if (trueAnswer.Contains(nextGuessChar) == false)
+                {
+                    outcomes.Add(LetterMatchOutcome.Grey);
+                    continue;
+                }
+                if (trueAnswer[i] == nextGuessChar)
+                {
+                    outcomes.Add(LetterMatchOutcome.Green);
+                    continue;
+                }
+                outcomes.Add(LetterMatchOutcome.Yellow);
+            }
+            TrimRepeatedLetters(guess, trueAnswer, outcomes);
+            return new WordleMask(outcomes);
+        }
+        static void TrimRepeatedLetters(string guess, string trueAnswer, List<LetterMatchOutcome> outcomes)
+        {
+            var guessHistogram = GenerateLetterHistogram(guess);
+            var answerHistogram = GenerateLetterHistogram(trueAnswer);
+            foreach (char c in trueAnswer)
+            {
+                if (guessHistogram.ContainsKey(c) == false)
+                {
+                    continue;
+                }
+                if (guessHistogram[c] > answerHistogram[c])
+                {
+                    int numberOfYellowsToTrim = guessHistogram[c] - answerHistogram[c];
+                    for (int i = 0; i < numberOfYellowsToTrim; i++)
+                    {
+                        TrimExtraYellow(guess, c, outcomes);
+                    }
+                }
+            }
+        }
+        static Dictionary<char, int> GenerateLetterHistogram(string input)
+        {
+            var output = new Dictionary<char, int>();
+            foreach (char c in input)
+            {
+                if (output.ContainsKey(c) == false)
+                {
+                    output.Add(c, 0);
+                }
+                output[c]++;
+            }
+            return output;
+        }
+        static void TrimExtraYellow(string guess, char repeatedChar, List<LetterMatchOutcome> outcomes)
+        {
+            for (int i = wordSize - 1; i >= 0; i--)
+            {
+                if (guess[i] == repeatedChar && outcomes[i]==LetterMatchOutcome.Yellow)
+                {
+                    outcomes[i] = LetterMatchOutcome.Grey;
+                    return;
+                }
+            }
+        }
+    }
+    public class WordleGuessInformation
+    {
+        public readonly string BestGuess;
+        public readonly string SecondBestGuess;
+        public readonly double ElapsedMilliseconds;
+        public readonly int NumberOfPossibilites;
+        public WordleGuessInformation(string guess, string? backupGuess, double time, int number)
+        {
+            BestGuess = guess;
+            if (backupGuess == null)
+            {
+                SecondBestGuess = guess;
+            }
+            else
+            {
+                SecondBestGuess = backupGuess;
+            }
+            ElapsedMilliseconds = time;
+            NumberOfPossibilites = number;
+        }
+
+    }
+    public enum LetterMatchOutcome
+    {
+        Grey,
+        Yellow,
+        Green
+    }
+    public class WordleMask
+    {
+        public readonly LetterMatchOutcome[] mask;
+        public WordleMask(List<LetterMatchOutcome> input)
+        {
+            mask = input.ToArray();
+        }
+        public override int GetHashCode()
+        {
+            int output = 0;
+            for (int i = 0; i < mask.Length; i++)
+            {
+                int nextDigit = (int)mask[i];
+                int placeMultiplier = (int)Math.Pow(3, i);
+                output += nextDigit * placeMultiplier;
+            }
+            return output;
+        }
+    }
+}
